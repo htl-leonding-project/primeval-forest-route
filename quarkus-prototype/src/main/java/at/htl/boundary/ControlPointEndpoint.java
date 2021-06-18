ac6d791fe6bf98c0e72837118d119fb6d0626940
Jakob Ratzenböck <gaglimon@gmail.com>
2021-06-18 10:53:46 +0200

    Ratzenböck - project status

--- quarkus-prototype/src/main/java/at/htl/boundary/ControlPointEndpoint.java
+++ quarkus-prototype/src/main/java/at/htl/boundary/ControlPointEndpoint.java
@@ -19,45 +19,45 @@ import java.util.Map;
 
 public class ControlPointEndpoint {
 
-    @Context
-    private ServletContext context;
-
-    @POST
-    @Path("/upload")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response uploadFile(MultipartFormDataInput input) throws IOException {
-
-        Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
-
-        // Get file data to save
-        List<InputPart> inputParts = uploadForm.get("attachment");
-
-        for (InputPart inputPart : inputParts) {
-            try {
-
-                MultivaluedMap<String, String> header = inputPart.getHeaders();
-                String fileName = getFileName(header);
-
-                // convert the uploaded file to inputstream
-                InputStream inputStream = inputPart.getBody(InputStream.class, null);
-
-                byte[] bytes = IOUtils.toByteArray(inputStream);
-
-                //String path = System.getProperty("user.home") + File.separator + "uploads";
-                File customDir = new File(Config.UPLOAD_FOLDER);
-
-                if (!customDir.exists()) {
-                    customDir.mkdir();
-                }
-                fileName = customDir.getCanonicalPath() + File.separator + fileName;
-                writeFile(bytes, fileName);
-
-                return Response.status(200).entity("Uploaded file name : " + fileName + " . <br/> <a href='" + context.getContextPath() + "'>Back</a>").build();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return null;
-    }
+//    @Context
+//    private ServletContext context;
+//
+//    @POST
+//    @Path("/upload")
+//    @Consumes(MediaType.MULTIPART_FORM_DATA)
+//    public Response uploadFile(MultipartFormDataInput input) throws IOException {
+//
+//        Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
+//
+//        // Get file data to save
+//        List<InputPart> inputParts = uploadForm.get("attachment");
+//
+//        for (InputPart inputPart : inputParts) {
+//            try {
+//
+//                MultivaluedMap<String, String> header = inputPart.getHeaders();
+//                String fileName = getFileName(header);
+//
+//                // convert the uploaded file to inputstream
+//                InputStream inputStream = inputPart.getBody(InputStream.class, null);
+//
+//                byte[] bytes = IOUtils.toByteArray(inputStream);
+//
+//                //String path = System.getProperty("user.home") + File.separator + "uploads";
+//                File customDir = new File(Config.UPLOAD_FOLDER);
+//
+//                if (!customDir.exists()) {
+//                    customDir.mkdir();
+//                }
+//                fileName = customDir.getCanonicalPath() + File.separator + fileName;
+//                writeFile(bytes, fileName);
+//
+//                return Response.status(200).entity("Uploaded file name : " + fileName + " . <br/> <a href='" + context.getContextPath() + "'>Back</a>").build();
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        return null;
+//    }
 }
